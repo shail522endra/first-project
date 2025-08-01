eedff680d263be5e9113ab150342d900efcb3fce
Shailendra Singh <144386789+shail522endra@users.noreply.github.com>
2025-08-01 23:49:22 +0530

    New Calculator

--- calculator.py
+++ calculator.py
@@ -4,6 +4,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
 # Example usage
 print("Add:", add(10, 5))
 print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10,5))
+print("Divide:", divide(10,5))
